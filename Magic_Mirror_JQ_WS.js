8c01ae7f22d6bd78959d1b027b0c2cde1aedf088
David Cai <davidcai2012@gmail.com>
2016-06-06 13:24:45 -0700

    Weather is now part of calendar table header
    Looks cooler. Also, moved calendar to top container, bottom is
    currently empty

--- Magic_Mirror_JQ_WS.js
+++ Magic_Mirror_JQ_WS.js
@@ -5,22 +5,6 @@ function updateMoment(){
 	$("#date").html(now.format('dddd' + ", " + 'MMMM Do'));
 }
 
-function updateWeather(){
-	var apiKey = 'bd0e23dbeb0c1b833c00b4b45d4fa188';
-	$.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=Tucson&mode=JSON&units=imperial&cnt=4&APPID=' + apiKey, function(weatherForecast){
-	// For the description, the access path differs from the given path on their site. You must add
-	// an index to the 'weather' element, because it's stored as a list for some reason.
-		$("#weather").html("Today ");
-		$("#weather").append(Math.round(weatherForecast.list[0].temp.min), " ", Math.round(weatherForecast.list[0].temp.max), " ", weatherForecast.list[0].weather[0].description, '<br>');
-		$("#weather").append(moment().add(1, 'd').format('dddd'), " ");
-		$("#weather").append(Math.round(weatherForecast.list[1].temp.min), " ", Math.round(weatherForecast.list[1].temp.max), " ", weatherForecast.list[1].weather[0].description,  '<br>');
-		$("#weather").append(moment().add(2, 'd').format('dddd'), " ");
-		$("#weather").append(Math.round(weatherForecast.list[2].temp.min), " ", Math.round(weatherForecast.list[2].temp.max), " ",weatherForecast.list[2].weather[0].description,  '<br>');
-		$("#weather").append(moment().add(3, 'd').format('dddd'), " ");
-		$("#weather").append(Math.round(weatherForecast.list[3].temp.min), " ", Math.round(weatherForecast.list[3].temp.max), " ",weatherForecast.list[3].weather[0].description, '<br>');
-	});
-}
-
 var clientId = '840122434097-gb87cdf08fupi69b262066jl68f9q1a4.apps.googleusercontent.com';
 var apiKey = 'AIzaSyDgUKhpF1UNxiTbGU-bS1FRPwyksg5M3I0';
 var scopes = 'https://www.googleapis.com/auth/calendar';
@@ -356,16 +340,27 @@ function calendarFrontEnd(allDayEvents, timedEvents){
 		if(start == "Invalid date"){start = " ";}
 		$("#plusFour-events").append(item.summary  + "  " + start + "<br>");
 	}
+
+	// Adding weather info to the table headers
+	var apiKey = 'bd0e23dbeb0c1b833c00b4b45d4fa188';
+	$.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=Tucson&mode=JSON&units=imperial&cnt=5&APPID=' + apiKey, function(weatherForecast){
+	// For the description, the access path differs from the given path on their site. You must add
+	// an index to the 'weather' element, because it's stored as a list for some reason.
+		$("#today-header").append("<br>" + Math.round(weatherForecast.list[0].temp.min), " ", Math.round(weatherForecast.list[0].temp.max), " ", weatherForecast.list[0].weather[0].description, '<br>');
+		$("#plusOne-header").append("<br>" + Math.round(weatherForecast.list[1].temp.min), " ", Math.round(weatherForecast.list[1].temp.max), " ", weatherForecast.list[1].weather[0].description,  '<br>');
+		$("#plusTwo-header").append("<br>" + Math.round(weatherForecast.list[2].temp.min), " ", Math.round(weatherForecast.list[2].temp.max), " ",weatherForecast.list[2].weather[0].description,  '<br>');
+		$("#plusThree-header").append("<br>" + Math.round(weatherForecast.list[3].temp.min), " ", Math.round(weatherForecast.list[3].temp.max), " ",weatherForecast.list[3].weather[0].description, '<br>');
+		$("#plusFour-header").append("<br>" + Math.round(weatherForecast.list[4].temp.min), " ", Math.round(weatherForecast.list[4].temp.max), " ",weatherForecast.list[4].weather[0].description, '<br>');
+	});
 }
 
 $(document).ready(function(){
 	updateMoment();
 	setInterval(updateMoment, 1000);
 
-	updateWeather();
-	setInterval(updateWeather, 600000);
-
 	handleClientLoad();
+
+	setInterval(makeApiCall, 600000);
 
 })
 
